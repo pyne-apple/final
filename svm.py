8f9f59d9787a0c46df1e9250884a860c36d2a101
John Bales <johntbalesg@gmail.com>
2017-11-22 13:21:16 -0600

    Read in attributes

--- svm.py
+++ svm.py
@@ -1,6 +1,22 @@
 # Support Vector Machine attempt
 import sys
 
+class attribute():
+    def __init__(self):
+        self.name = None  
+        self.type = None  
+        self.rangeBeg = None
+        self.rangeEnd = None
+        self.offset = None
+    
+    def print(self):
+        print("Name:", self.name, end='')
+        print(", Type:", self.type, end='')
+        print(", Range:[", self.rangeBeg, end='')
+        print(",", self.rangeEnd, end='')
+        print("], Offset:", self.offset)
+        pass
+
 
 # python svm.py train.txt attr.txt
 def main(): 
@@ -13,12 +29,43 @@ def main():
 
     # Reading in data from training file
     with open(trainingFileName) as trainingFile, open(attributeFileName) as attributeFile:
+        attributeList = []  # List of attribute objects
+        for line in attributeFile:
+            newAttr = attribute()
+
+            splitValues = line.rstrip(" .\n").split()
+            attrName = splitValues[0] 
+            attrVal1 = splitValues[1] 
+            attrVal2 = None
+            if len(splitValues) > 2:
+                attrVal2 = splitValues[2]
+
+            newAttr.name = attrName.rstrip(" :")  # Remove the ':' from the end
+            
+            if (attrVal1 == "cont"):
+                newAttr.type = "cont" # continuous
+                if (attrVal2 is not None):
+                    newAttr.rangeBeg, newAttr.rangeEnd = (int(x) for x in attrVal2.split(".."))
+            elif (attrVal1 == "0,1"):
+                newAttr.type = "bit"  
+            else:
+                newAttr.type = "offset" # A quick hack to handle 2001,2002,2003,2004,2005,2006
+                newAttr.offset = 2001
+                newAttr.rangeBeg = 0
+                newAttr.rangeEnd = 5
+
+            attributeList.append(newAttr)
+        
+        # Check attributes where read correctly
+        # for attr in attributeList:
+        #     attr.print()
+
         trainingLists = []
         for line in trainingFile:
             splitValues = list(x for x in line.split())
             trainingLists.append(splitValues)
 
-        print(trainingLists[:10])
+        # print(trainingLists[:10])
 
 
 
